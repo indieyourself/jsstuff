aa10914dbb5834a767b43714843912c4a32043c4
HanWenfang <eehanwenfang@gmail.com>
2016-09-18 10:29:41 +0800

    node kafka producer

---
+++ node-kafka.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const Kafka = require('no-kafka');
+const producer = new Kafka.Producer( {connectionString: 'kafka://192.168.10.87:9092' } );
+
+const message = {
+	topic: "test",
+	partition: 0,
+	message: {
+		value: "Hello From NodeJS!"
+	}
+};
+
+return producer.init().then( function() {
+	return producer.send( message, {
+		retries: {
+			attempts: 2,
+			delay: 100
+		}
+	});
+} )
+.then( function(result) {
+	console.log(result);
+})
+
+
+
+
+
+
