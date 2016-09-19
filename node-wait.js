a32d2fbe96bea69a92a32bbd4db7bf0493e3ebc6
HanWenfang <eehanwenfang@gmail.com>
2016-09-19 11:14:15 +0800

    wait

---
+++ node-wait.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const net = require('net');
+
+console.log("node wait test");
+
+function Producer( options ) {
+	this.options = options;
+
+	// Connection
+	this.socket = new net.Socket();
+
+}
+
+Producer.prototype.init = function() {
+	this.socket.connect( this.options.port, this.options.host , function() {
+		console.log("connected");
+	});
+}
+
+
+
+const producer = new Producer( { host: '192.168.10.87', port: 9092 } );
+
+// WILL WAIT IN EPOLL
+producer.init();
+
