34018aecfe80cc14ef0b6ea7c62b7768a4577b97
HanWenfang <eehanwenfang@gmail.com>
2016-09-19 20:09:32 +0800

    mongodb

--- node-mongodb.js
+++ node-mongodb.js
@@ -5,19 +5,20 @@ const MongoClient = require('mongodb').MongoClient;
 var mongoDB;
 
 MongoClient.connect("mongodb://192.168.10.87:27017/test", function(err, db) {
-	if ( !err) {
+	if ( err !== null ) {
 		console.log("mongodb connection error");
-		return -1;
+		process.exit(-1);
 	};
 
 	mongoDB = db;
-});
 
-mongoDB.collection('user').insertOne(
+	mongoDB.collection('user').insertOne(
 		{ "name": "x-one" },
 		function(err, result) {
 			console.log(result);
 		}
-	)
-}
+	);
+});
+
+
 
