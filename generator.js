d8ea348da21c1fc7645d72c1b5e697facdaa8687
HanWenfang <eehanwenfang@gmail.com>
2016-10-14 11:01:48 +0800

    generator

--- generator.js
+++ generator.js
@@ -65,3 +65,63 @@ console.log( gen.next().value );
 
 console.log( gen.next().value );
 console.log( gen.next().value );
+
+
+/* Didn't Work!!
+function idMaker() {
+	var index = 0;
+	while (true) {
+		yield index++;
+	}
+}
+
+function* genClass( callback ) {
+	callback();
+}
+
+var genInstnce = genClass( idMaker );
+
+console.log('======genClass:idMaker======');
+console.log( genInstnce.next().value );
+console.log( genInstnce.next().value );
+console.log( genInstnce.next().value );
+console.log( genInstnce.next().value );
+console.log( genInstnce.next().value );
+console.log( genInstnce.next().value );
+console.log( genInstnce.next().value );
+*/
+
+/*
+	tasklet => function*
+*/
+
+function one() {
+	return 1;
+}
+
+function two() {
+	return 2;
+}
+
+function* numMaker( callback ) {
+	while(true) {
+		yield callback();
+	}
+}
+
+var oneGen = numMaker(one);
+var twoGen = numMaker(two);
+/*
+	scheduler
+*/
+function scheduler() {
+	console.log('======scheduler======');
+	console.log( oneGen.next().value );
+	console.log( twoGen.next().value );
+	console.log( oneGen.next().value );
+	console.log( twoGen.next().value );
+	console.log( oneGen.next().value );
+	console.log( twoGen.next().value );
+}
+
+scheduler();
